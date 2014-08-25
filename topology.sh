7c948c5da64ae2c3f476c4fa31f0d9eaf95be971
Ben Hyde <bhyde@pobox.com>
2014-08-25 17:34:02 -0400

    I got the cl-launch script to work.

--- topology.sh
+++ topology.sh
@@ -1,11 +1,33 @@
-#!/usr/local/bin/cl -l ccl -S ~/w/selected-asd -s yed-lisp -E main --
-# -*- mode: common-lisp -*-
-(in-package #:cl-user)
+#!/usr/local/bin/cl -S ~/w/selected-asd -s yed-lisp -E main -X --
+":" ; # -*- mode: common-lisp -*-
 (defun main (argv)
-  (multiple-value-bind (nodes edges)
-      (yed-lisp:extract-topology (yed-lisp:load-graph (pop argv)))
-    (flet ((node-name (node) (second (assoc node nodes))))
-      (loop for (nil from to) in edges
-         do (format t "~&~A -> ~A"
-                    (node-name from) (node-name to))))))
+  (handler-case
+      (flet
+          ((read-and-report (graph-namestring)
+             (multiple-value-bind (nodes edges)
+                 (yed-lisp:extract-topology 
+                  (yed-lisp:load-graph graph-namestring))
+               (flet ((node-name (node) (second (assoc node nodes))))
+                 (loop for (nil from to) in edges
+                    do (format t "~&~A -> ~A"
+                               (node-name from) (node-name to))))))
+           (get-graph-namestring ()
+             (optima:match argv
+               ((or (list "-h")
+                    (list "--help"))
+                (format t "Usage: <prog> <graphML file>")
+                (uiop:quit))
+               ((list (optima:guard graph-namestring (stringp graph-namestring)))
+                (unless (probe-file graph-namestring)
+                  (format *error-output* "File not found: ~A" graph-namestring)
+                  (uiop:quit -1))
+                graph-namestring)
+               (_
+                (format *error-output* "~&ERROR: malform args, try --help")
+                (uiop:quit -1)))))
+        (read-and-report (get-graph-namestring)))
+    (error (e) 
+      (format *error-output* "~&ERROR: ~A" e)
+      (uiop:quit -1))))
+     
 
